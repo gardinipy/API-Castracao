3aefd6ae45b3e4815c13e3782991fd26b8949438
Gardini <trjakira@proton.me>
2025-05-29 10:38:40 -0300

    Gemini update

--- apiCastracao/src/main/java/br/com/fatec/apiCastracao/dto/ItensManejoDTO.java
+++ apiCastracao/src/main/java/br/com/fatec/apiCastracao/dto/ItensManejoDTO.java
@@ -1,4 +1,10 @@
 package br.com.fatec.apiCastracao.dto;
 
-public record ItensManejoDTO() {
+import jakarta.validation.constraints.NotNull;
+
+// Original: public record ItensManejoDTO() { }
+public record ItensManejoDTO(
+    @NotNull Integer agendamentoId, // ID do Agendamento relacionado
+    @NotNull Integer manejoId       // ID do Manejo relacionado
+) {
 }
