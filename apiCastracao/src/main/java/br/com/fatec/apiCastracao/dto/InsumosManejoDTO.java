9e43be7f2a0240a8e5cb968059aaa1bc903e8c0c
Gardini <trjakira@proton.me>
2025-05-29 10:36:24 -0300

    Update InsumosManejoDTO.java

--- apiCastracao/src/main/java/br/com/fatec/apiCastracao/dto/InsumosManejoDTO.java
+++ apiCastracao/src/main/java/br/com/fatec/apiCastracao/dto/InsumosManejoDTO.java
@@ -1,4 +1,12 @@
 package br.com.fatec.apiCastracao.dto;
 
-public record InsumosManejoDTO() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+
+// Original: public record InsumosManejoDTO() { }
+public record InsumosManejoDTO(
+    @NotNull Integer insumoId, // ID do Insumo relacionado
+    @NotNull Integer manejoId,   // ID do Manejo relacionado
+    @NotNull @Min(1) Integer qtde       // Quantidade não pode ser nula e deve ser pelo menos 1
+) {
 }
